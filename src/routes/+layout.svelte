bfdbe472abd4eee7a9c404a1e445b85a40bfc8fe
daniel <daniel@acudac.com>
2025-02-25 12:44:19 +0200

    md3 colours, tailwind and modewatcher setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import '../app.css';
+	import { ModeWatcher } from 'mode-watcher';
+	let { children } = $props();
+
+	const contrast: '' | '-high-contrast' | '-medium-contrast' = '';
+</script>
+
+<ModeWatcher lightClassNames={['light' + contrast]} darkClassNames={['dark' + contrast]} />
+{@render children()}
