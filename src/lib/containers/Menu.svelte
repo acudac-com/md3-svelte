3d3225b1b1f57e5e76fd990ffd31cae2f48dd6d9
daniel.van.niekerk@alisx.com <daniel.van.niekerk@alisx.com>
2025-03-08 14:28:15 +0200

    feat: Menu

---
+++ src/lib/containers/Menu.svelte
@@ -0,0 +1,29 @@
+<script module lang="ts">
+	export interface MenuProps {
+		trigger: Snippet;
+		children: Snippet;
+		open?: boolean;
+		side?: 'top' | 'bottom' | 'right' | 'left';
+		sideOffset?: number;
+		class?: string | string[];
+		disabled?: boolean;
+	}
+</script>
+
+<script lang="ts">
+	import { DropdownMenu } from 'bits-ui';
+	import type { Snippet } from 'svelte';
+
+	let { open = $bindable(), sideOffset = 4, ...p }: MenuProps = $props();
+</script>
+
+<DropdownMenu.Root bind:open>
+	<DropdownMenu.Trigger class={p.class} disabled={p.disabled}>
+		{@render p.trigger()}
+	</DropdownMenu.Trigger>
+	<DropdownMenu.Portal>
+		<DropdownMenu.Content {sideOffset} side={p.side}>
+			{@render p.children()}
+		</DropdownMenu.Content>
+	</DropdownMenu.Portal>
+</DropdownMenu.Root>
--- src/routes/components/containers/+page.svelte
+++ src/routes/components/containers/+page.svelte
@@ -1,8 +1,10 @@
 <script>
-	import { Button, Dialog } from '$lib';
+	import { Button, Dialog, Menu, Row } from '$lib';
+	import Card from '$lib/clickables/Card.svelte';
 	import IconButton from '$lib/clickables/IconButton.svelte';
 	import { mdiSettings } from '$lib/icons';
 	let open = $state(false);
+	let menuOpen = $state(false);
 </script>
 
 <IconButton icon={mdiSettings} bind:toggled={open} />
@@ -17,3 +19,21 @@
 		text="Close"
 	/>
 </Dialog>
+
+<Menu class="ml-0" sideOffset={4} bind:open={menuOpen}>
+	{#snippet trigger()}
+		<IconButton icon={mdiSettings} bind:toggled={menuOpen} />
+	{/snippet}
+	<Card class="px-8">
+		<p class="headline-medium">Headline</p>
+		<Row justify="end" gaps>
+			<Button
+				text="Cancel"
+				onclick={() => {
+					menuOpen = false;
+				}}
+			/>
+			<Button text="Save" />
+		</Row>
+	</Card>
+</Menu>
