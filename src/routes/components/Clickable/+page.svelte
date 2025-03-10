6465166e2c7aae71c0630dad95a68f52a84a161c
daniel.van.niekerk@alisx.com <daniel.van.niekerk@alisx.com>
2025-03-10 10:48:18 +0200

    chore: Clickable examples

---
+++ src/routes/components/Clickable/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { ChipDialog, Col, Divider, Row } from '$lib';
+	import CircularProgress from '$lib/components/CircularProgress.svelte';
+	import Clickable from '$lib/components/Clickable.svelte';
+	import Example from '$lib/internal/Example.svelte';
+</script>
+
+<Col wFull hFit gaps>
+	<p class="body-small">
+		Provides a low level component for accepting click inputs to navigate (href) or do some action
+		(onclick). Provides a ripple effect.
+	</p>
+	<Divider />
+	<p class="title-small">Examples</p>
+
+	<Example
+		title=""
+		code={`<Clickable class="size-full hover:bg-primary-container" href="?id=1"
+	>List item 1</Clickable>
+<Clickable class="size-full hover:bg-primary-container" href="?id=2"
+	>List item 2</Clickable>`}
+	>
+		<Clickable class="size-full hover:bg-primary-container" href="?id=1">List item 1</Clickable>
+		<Clickable class="size-full hover:bg-primary-container" href="?id=2">List item 2</Clickable>
+	</Example>
+</Col>
