cbd1786a236659c688ce7e7f6cdf8d613722df45
daniel.van.niekerk@alisx.com <daniel.van.niekerk@alisx.com>
2025-03-10 20:58:27 +0200

    chore: npm version

--- src/routes/guide/+layout.svelte
+++ src/routes/guide/+layout.svelte
@@ -379,40 +379,40 @@ export const pngGoogleMeet: Image = {
 			2. Select prettier and tailwindcss as addons when prompted. Select typography when prompted
 			for tailwindcss plugins. Select your prefered package manager.
 		</p>
+		<p>3. Run `pnpm add md3-svelte`</p>
 		<p>
-			3. Pick and export your theme as CSS using the <a
-				class="text-primary"
-				href="https://material-foundation.github.io/material-theme-builder/">MD3 Theme Builder</a
+			4. Pick and export your theme as CSS using the <Link
+				href="https://material-foundation.github.io/material-theme-builder/">MD3 Theme Builder</Link
 			>. Place the exported .css files in 'src/theme' and remove the 'rgb(...)' wrapper for each
 			color. The latter is necessary to apply background opacity to these colours. To make this
 			easier you can add and run the following script in your package.json:
 		</p>
 		<pre
 			class="label-small pl-4 text-tertiary">{`"theme": "find src/theme -type f -exec sed -i '' -e 's/rgb(//g' -e 's/);/;/g' {} +"`}</pre>
-		<p>4. Override the following files' content with the provided code.</p>
+		<p>5. Override the following files' content with the provided code.</p>
 		<Row class="pl-4" gaps>
 			<p>src/app.html</p>
 			<IconMenu icon={mdiCode} class="size-[30px]">
 				<pre
-					class="mr-4 max-h-[500px] max-w-[400px] overflow-scroll rounded-sm bg-surface p-1 shadow-l1">{appHtml}</pre>
+					class="mr-4 max-h-[400px] max-w-[400px] overflow-scroll rounded-sm bg-surface p-1 shadow-l1">{appHtml}</pre>
 			</IconMenu>
 		</Row>
 		<Row class="pl-4" gaps>
 			<p>src/app.css</p>
 			<IconMenu icon={mdiCode} class="size-[30px]">
 				<pre
-					class="mr-4 max-h-[500px] max-w-[400px] overflow-scroll rounded-sm bg-surface p-1 shadow-l1">{appCss}</pre>
+					class="mr-4 max-h-[400px] max-w-[400px] overflow-scroll rounded-sm bg-surface p-1 shadow-l1">{appCss}</pre>
 			</IconMenu>
 		</Row>
 		<Row class="pl-4" gaps>
 			<p>tailwind.config.ts</p>
 			<IconMenu icon={mdiCode} class="size-[30px]">
 				<pre
-					class="mr-4 max-h-[500px] max-w-[400px] overflow-scroll rounded-sm bg-surface p-1 shadow-l1">{tailwindConfig}</pre>
+					class="mr-4 max-h-[400px] max-w-[400px] overflow-scroll rounded-sm bg-surface p-1 shadow-l1">{tailwindConfig}</pre>
 			</IconMenu>
 		</Row>
 		<p>
-			5. Combine <Link href="/layouts">Layouts</Link> and <Link href="/components">Components</Link>
+			6. Combine <Link href="/layouts">Layouts</Link> and <Link href="/components">Components</Link>
 			to build your MD3 inspired web app!
 		</p>
 	</Container>
@@ -422,9 +422,7 @@ export const pngGoogleMeet: Image = {
 		<p>
 			All components with 'icon' attributes accept both SVGs and Images. The simplest pattern for
 			adding icons is to create an icons.ts file in your lib folder and export all your icons from
-			there. You can find icons at <a class="text-primary" href="https://icon-sets.iconify.design/"
-				>Iconify</a
-			>
+			there. You can find icons at <Link href="https://icon-sets.iconify.design/">Iconify</Link>
 			and simply paste the icons' raw JSON data in the icons.ts file. Refer to <Menu
 				class="font-bold text-primary"
 			>
