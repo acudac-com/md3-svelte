f29157916895da0e9874bbf847b8454c001d031a
daniel.van.niekerk@alisx.com <daniel.van.niekerk@alisx.com>
2025-03-10 18:59:28 +0200

    chore: Slider example

---
+++ src/routes/components/Switch/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { Col, Divider, Switch } from '$lib';
+	import Example from '$lib/internal/Example.svelte';
+	let checked = $state(true);
+</script>
+
+<Col wFull hFit gaps>
+	<p class="body-small">Switches toggle the selection of an item on or off.</p>
+	<Divider />
+	<p class="title-small">Examples</p>
+
+	<Example title="" code={`<Switch bind:checked/>`}>
+		<Switch bind:checked />
+	</Example>
+</Col>
