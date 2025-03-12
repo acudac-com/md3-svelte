999bf596580cfa20ac37c48e7cef91fbc299b0dd
daniel.van.niekerk@alisx.com <daniel.van.niekerk@alisx.com>
2025-03-12 16:29:29 +0200

    fix: DatePicker focusPicker styling

--- src/lib/components/_picker/FocusPicker.svelte
+++ src/lib/components/_picker/FocusPicker.svelte
@@ -13,14 +13,14 @@
 <div class="m3-container">
 	{#each options as { id, name, selected }}
 		<button
-			class="m3-font-body-large"
-			on:click={() => dispatch('chosen', id)}
+			class="body-small align-center relative inline-flex h-[50px] shrink-0 cursor-pointer items-center justify-between bg-transparent px-10 text-on-surface"
+			onclick={() => dispatch('chosen', id)}
 			use:conditionalScroll={selected}
 		>
+			{name}
 			{#if selected}
 				<Icon icon={mdiCheck} />
 			{/if}
-			{name}
 		</button>
 	{/each}
 </div>
@@ -31,38 +31,19 @@
 		flex-direction: column;
 		flex: 1 0;
 		overflow: auto;
-		margin-bottom: 1.25rem;
 	}
 	button {
-		display: inline-flex;
-		align-items: center;
-		height: 3rem;
-		padding-left: 3.5rem;
-		flex-shrink: 0;
-
-		background-color: transparent;
-		color: rgb(var(--m3-scheme-on-surface));
-		border: none;
-		position: relative;
 		-webkit-tap-highlight-color: transparent;
-		cursor: pointer;
 		transition: all 200ms;
 	}
-	button :global(svg) {
-		width: 1.5rem;
-		height: 1.5rem;
-		position: absolute;
-		left: 1rem;
-		top: 50%;
-		transform: translateY(-50%);
-	}
+
 	@media (hover: hover) {
 		button:hover {
-			background-color: rgb(var(--m3-scheme-on-surface) / 0.08);
+			background-color: rgb(var(--md-sys-color-on-surface) / 0.08);
 		}
 	}
 	button:focus-visible,
 	button:active {
-		background-color: rgb(var(--m3-scheme-on-surface) / 0.12);
+		background-color: rgb(var(--md-sys-color-on-surface) / 0.12);
 	}
 </style>
